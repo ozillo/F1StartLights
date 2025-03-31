959f82eb4ced4922f8ac3abe56b6063c9a09cec3
ozillo <marcmateo25@gmail.com>
2025-04-01 00:56:32 +0200

    responsive moods and add font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import F1StartLights from "./components/F1StartLights";
 import InstructionsModal from "./components/InstructionsModal";
+import { useState } from "react";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(true); // Inicialmente, el modal está abierto
+
+  const handleClose = () => {
+    setIsModalOpen(false); // Al hacer clic en "Entendido", el modal se cierra
+  };
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900">
-      <InstructionsModal/>
+      {/* Si el modal está abierto, se muestra */}
+      {isModalOpen && <InstructionsModal onClose={handleClose} />}
+      
       <F1StartLights />
     </div>
   );
